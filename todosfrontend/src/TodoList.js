5a487b3ae5c4e2aed3d63b3c5956a2734da9faef
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-03-07 20:19:38 -0700

    Refactor addTodos as async function, add let newTodo to await apiCalls.getTodos function

--- todosfrontend/src/TodoList.js
+++ todosfrontend/src/TodoList.js
@@ -22,31 +22,9 @@ class TodoList extends Component {
     this.setState({todos});
   }
 
-  addTodo(val) {
-    fetch(APIURL, {
-      method: "POST",
-      headers: new Headers({
-        "Content-Type": "application/json",
-      }),
-      body: JSON.stringify({name: val})
-    })
-    .then(resp => {
-      if(!resp.ok) {
-        if(resp.status >=400 && resp.status < 500) {
-          return resp.json().then(data => {
-            let err = {errorMessage: data.message};
-            throw err;
-          })
-        } else {
-          let err = {errorMessage: "Please try again later, server is not responding."};
-          throw err;
-        }
-      }
-      return resp.json();
-    })
-    .then(newTodo => {
-      this.setState({todos: [...this.state.todos, newTodo]})
-    })
+  async addTodo(val) {
+    let newTodo = await apiCalls.createTodo(val);
+    this.setState({todos: [...this.state.todos, newTodo]})
   }
 
   deleteTodo(id) {
